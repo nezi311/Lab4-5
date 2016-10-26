9e5b116ce41dbc9633f842dc4dc317c588d5d96b
n3z1 <dawid.dominiak.94@gmail.com>
2016-10-26 13:37:09 +0200

    Poprawa literówki

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <body>
     <ul>
-        <li><a href="formulasz.html">Formularz</a></li>
+        <li><a href="formularz.html">Formularz</a></li>
         <li><a href="sale.php">Wszystie sale</a></li>
     </ul>
 
